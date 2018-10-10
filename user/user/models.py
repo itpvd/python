7e52acc5887e72281b7aa97d356081e5027778c4
itpvd <phamvietdaoit@gmail.com>
2018-10-10 22:04:39 +0700

    1.1

--- user/user/models.py
+++ user/user/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from user import db, login_manager
 from flask_login import UserMixin
@@ -11,4 +12,9 @@ class User(db.Model, UserMixin):
 	username = db.Column(db.String(64), index=True, unique=True)
 	password = db.Column(db.String(128))
 	role = db.Column(db.String(4))
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
 
--- user/user/routes.py
+++ user/user/routes.py
@@ -1,37 +1,39 @@
 from flask import Flask, render_template,url_for,flash,redirect,request
 from user import app,db
-from user.forms import loginForm,registerForm
-from user.models import User
+from user.models import User,Post
 from flask_login import login_user,logout_user
 
+#--LOGIN function--
+#form login
 @app.route("/")
-@app.route("/login", methods=['GET','POST'])
-def login():
-	form = loginForm()
-	return render_template('login.html',title='login',form = form)
-
-@app.route("/checkLogin", methods=['GET','POST'])
+@app.route("/formLogin")
+def formLogin():
+	return render_template('login.html',title='login')
+#check login
+@app.route("/checkLogin", methods=['POST'])
 def checkLogin():
       result = request.form
       user = User.query.filter_by(username=result['username']).first()
       if user and user.password == result['password'] and user.role=='user':
-            login_user(user,remember = True)
+            login_user(user,remember = False)
             flash('Hello %s! welecome to website'%result['username'])
-            return render_template("news.html",result = result)
+            return redirect('viewPost')
       elif user and user.password == result['password'] and user.role=='admin':
-            login_user(user,remember = True)
+            login_user(user,remember = False)
             flash('Hello %s, welecome to website'%result['username'])
-            return redirect('listuser')
+            return redirect('listUser')
       else:
             flash('User name or password is incorrect')
-            return redirect('login')
+            return redirect('formLogin')
 
 
-@app.route("/register",methods=['GET','POST'])
-def register():
-	form = registerForm()
-	return render_template('register.html',title='register',form = form)
-@app.route("/checkRegister", methods=['GET','POST'])
+#--REGISTER function--
+#form register
+@app.route("/formRegister")
+def formRegister():
+	return render_template('register.html',title='register')
+#check register
+@app.route("/checkRegister", methods=['POST'])
 def checkRegister():
       result = request.form
       user = User.query.filter_by(username=result['username']).first()
@@ -43,51 +45,122 @@ def checkRegister():
             return render_template("register.html")
       else:
             flash('Account: %s created failed'%result['username'])
-            return redirect('register')
-
-
-
+            return redirect('formRegister')
+#Logout
 @app.route("/logout")
 def logout():
       logout_user()
-      form = loginForm()
-      return render_template('login.html',title='login',form = form)
+      return redirect('formLogin')
 
 
-@app.route("/listuser", methods=['GET','POST'])
-def listuser():
+#--USER MANAGER function--
+#show list user
+@app.route("/listUser")
+def listUser():
       result = User.query.all()
+      page = request.args.get('page', 1, type=int)
+      result = User.query.paginate(page=page, per_page=2)
       return render_template("admin_user.html",result = result)
-
-
-@app.route("/deleteuser", methods=['GET','POST'])
-def deleteuser():
+#delete user 
+@app.route("/deleteUser", methods=['GET'])
+def deleteUser():
       id = request.args['id']
       User.query.filter_by(id=id).delete()
       db.session.commit()
-      return redirect('listuser')
-
-
-@app.route("/adduser",methods=['GET','POST'])
-def adduser():
-      form = registerForm()
-      return render_template('admin_user_add.html',form = form)
-@app.route("/checkadduser", methods=['GET','POST'])
-def checkadduser():
+      return redirect('listUser')
+#show form add user
+@app.route("/formAddUser")
+def formAddUser():
+      return render_template('admin_user_add.html')
+#create new user
+@app.route("/addUser", methods=['POST'])
+def addUser():
       result = request.form
       if result['password']== result['confilmpass']:
             user = User(username=result['username'],password=result['password'],role=result['role'])
             db.session.add(user)
             db.session.commit()
-            return redirect('listuser')
+            return redirect('listUser')
       else:
-            return redirect('adduser')
-
-
-
-@app.route("/edituser",methods=['GET','POST'])
-def edituser():
+            return redirect('formAddUser')
+#show form edit user
+@app.route("/formEditUser", methods=['GET'])
+def formEditUser():
       id = request.args['id']
       user = User.query.filter_by(id=id).first()
-      form = registerForm()
-      return render_template('admin_user_update.html',form = form)
+      return render_template('admin_user_update.html',user=user)
+#update user in database
+@app.route("/updateUser",methods=['POST'])
+def updateUser():
+      result = request.form
+      id = result['id']
+      user = User.query.filter_by(id=id).first()
+      user.username = result['username']
+      user.password = result['password']
+      user.role = result['role']
+      db.session.commit()
+      return redirect('listUser')
+
+
+#--POST MANAGER function--
+#show list all post
+@app.route("/listPost")
+def listPost():
+      result = Post.query.all()
+      page = request.args.get('page', 1, type=int)
+      result = Post.query.paginate(page=page, per_page=2)
+      return render_template("admin_post.html",result = result)
+#delete post
+@app.route("/deletePost", methods=['GET'])
+def deletePost():
+      id = request.args['id']
+      Post.query.filter_by(id=id).delete()
+      db.session.commit()
+      return redirect('listPost')
+#show form add post
+@app.route("/formAddPost")
+def formAddPost():
+      return render_template('admin_post_add.html')
+#create new post
+@app.route("/addPost", methods=['POST'])
+def addPost():
+      result = request.form
+      if result['title']!='' and result['content']!='':
+            post = Post(title=result['title'],content=result['content'])
+            db.session.add(post)
+            db.session.commit()
+            return redirect('listPost')
+      else:
+            return redirect('formAddPost')
+#show form edit post
+@app.route("/formEditPost",methods=['GET'])
+def formEditPost():
+      id = request.args['id']
+      post = Post.query.filter_by(id=id).first()
+      return render_template('admin_post_update.html',post=post)
+#update post in database
+@app.route("/updatePost",methods=['POST'])
+def updatePost():
+      result = request.form
+      id = result['id']
+      post = Post.query.filter_by(id=id).first()
+      post.title = result['title']
+      post.content = result['content']
+      db.session.commit()
+      return redirect('listPost')
+
+#POST VIEW function
+#show list all post
+@app.route("/viewPost")
+def viewPost():
+      result = Post.query.all()
+      page = request.args.get('page', 1, type=int)
+      result = Post.query.paginate(page=page, per_page=2)
+      return render_template("news.html",result = result)
+#show detail post
+@app.route("/detailPost")
+def detailPost():
+      id = request.args['id']
+      result = Post.query.filter_by(id=id).first()
+      return render_template("detail_post.html",result = result)
+
